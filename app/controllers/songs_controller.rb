126b44f9940d68e15c33d9bb9ed0f420d0c1e195
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-02-29 11:19:29 -0800

    Initial commit

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,27 @@
+class SongsController < ApplicationController
+  def songs
+    @songs = Song.all
+    render :index
+  end
+
+  def create
+    @song = Song.create(
+      title: params["title"],
+      album: params["album"],
+      artist: params["artist"],
+      year: params["year"],
+    )
+    render :show
+  end
+
+  def update
+    @song = Song.find_by(id: params[:id])
+    @song.update(
+      title: params["title"] || @song.title,
+      album: params["album"] || @song.album,
+      artist: params["artist"] || @song.artist,
+      year: params { "year" } || @song.year,
+    )
+    render :show
+  end
+end
